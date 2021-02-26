599e43f3800e2707cc25d328962c805cd9104d42
VisalYun <yunvisal18@kit.edu.kh>
2021-02-26 15:59:30 +0700

    add add-todo feature, search feature and hide feature

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-const list = document.querySelector('#todo-list ul');
+document.addEventListener('DOMContentLoaded', function(e){
+    const list = document.querySelector('#todo-list ul');
 
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentNode;
-        list.removeChild(li);
-    }
+    list.addEventListener('click', function(e){
+        if(e.target.className == 'delete'){
+            const li = e.target.parentNode;
+            list.removeChild(li);
+        }
+    });
+
+    const add_todo = document.forms['add-todo'];
+
+    add_todo.addEventListener('submit', function(e){
+        e.preventDefault();
+        const value = add_todo.querySelector('input[type="text"]').value;
+        console.log(value);
+
+        const li = document.createElement('li');
+        const todo = document.createElement('span');
+        const deleteBtn = document.createElement('span');
+
+        todo.classList.add('name');
+        deleteBtn.classList.add('delete');
+
+        todo.textContent = value;
+        deleteBtn.textContent = 'Delete';
+
+        li.appendChild(todo);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+    })
+
+    const hideCheckBox = document.querySelector('#hide');
+    hideCheckBox.addEventListener('change', function(e){
+        if(hideCheckBox.checked){
+            list.style.display = "none";
+        }
+        else{
+            list.style.display = "initial";
+        }
+    })
+
+    const searchBar = document.forms['search-todos'].querySelector('input');
+    searchBar.addEventListener('keyup', function(e){
+        const term = e.target.value.toLowerCase();
+        const book = document.getElementsByTagName('li');
+        Array.from(book).forEach(function(book){
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) != -1){
+                book.style.display = 'block';
+            }
+            else{
+                book.style.display = 'none';
+            }
+        })
+    });
 });
-
-const add_todo = document.forms['add-todo'];
-
-add_todo.addEventListener('submit', function(e){
-    e.preventDefault();
-    const value = add_todo.querySelector('input[type="text"]').value;
-    console.log(value);
-})
--- readme.md
+++ readme.md
@@ -40,6 +40,18 @@ All Elements that match the exactly specifies CSS selector are selector. **Note*
 var lis = document.querySelectorAll('#id_name .class_name tag_name')
 ```
 
+## Create Element
+### createElement('tag_name')
+Use to create a custom element in the Js runtime
+```
+var ele = document.createElement('tag_name')
+```
+### appendChild('child_name')
+Use to nested the child element inside the specifies element
+```
+parentEle.appendChild('childEle')
+```
+
 ## Manipulate Element Content:
 ### element.textContent
 Access to the text content of the particular element.
@@ -87,3 +99,39 @@ event_name: https://www.w3schools.com/jsref/dom_obj_event.asp
 e: Event object. Find out more on https://www.w3schools.com/jsref/obj_events.asp
 ### Event Bubbling
 **Disadvantage of the above event**: if the new element is added during the runtime, it won't get any event listener. The concept of event bubbling is to add an event listener to the parent of the element that we want to interact with. Once we click its child the element the event will bubble up to its parent.
+
+## Style
+### Accessing the CSS style property
+```
+ele.style.color = "red";
+ele.style.margin-top = "10px";
+```
+### Add the class to the element
+```
+ele.classList.add('class_name');
+```
+This will append the class to the existing class inside the element
+### Remove class from the element
+```
+ele.classList.remove('class_name');
+```
+
+## Attribute
+### get attribute of the element
+return the value of the attribute if there is an attribute inside the element, null if not.
+```
+ele.getAttribute('attribute_name');
+```
+### set attribute of the element
+```
+ele.setAttribute('attribute_name', 'value');
+```
+### check if the element has a specifies attribute
+return true if has, false if not.
+```
+ele.hasAttribute('attribute_name');
+```
+### remove attribute from the element
+```
+ele.removeAttribute('attribute_name');
+```
